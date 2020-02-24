16754f0dc6d752a0a2d19ef7e442cb702c596d27
nurimantjiptarto <nkt5@njit.edu>
2020-02-24 15:39:15 -0500

    fixing error

--- Statistics.js
+++ Statistics.js
@@ -12,7 +12,7 @@ class Statistics extends Calculator {
     // either we want to use this directly from calculator or we can just use Math Operation
     //which ever we want
 
-    // VarianceV2 (a,b) {
+    //VarianceV2 (a,b) {
         //return this.result = MathOperation.subtract(a,b);
 
 
