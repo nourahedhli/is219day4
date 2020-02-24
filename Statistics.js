b1e08eb8a6afd91814709ecd553077faac531d67
Noura.Hedhli <nh98@njit.edu>
2020-02-24 15:06:05 -0500

    Feature: Stats

---
+++ Statistics.js
@@ -0,0 +1,34 @@
+const Calculator = require('./Calculator');
+//const MathOperation = require('./Calculator');
+
+class Statistics extends Calculator {
+// we using this by extending calculator
+
+    Variance (a,b) {
+
+        this.subtract(a,b);
+
+    }
+    // either we want to use this directly from calculator or we can just use Math Operation
+    //which ever we want
+
+    // VarianceV2 (a,b) {
+        //return this.result = MathOperation.subtract(a,b);
+
+
+    //}
+
+
+    Mean (values){
+
+        let sum = this.Add(values);
+        let numValues = values.length;
+        return this.Divide(sum,numValues);
+
+
+    }
+
+}
+
+module.exports =Statistics;
+
